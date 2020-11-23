a7fffc15843f7b62bc056d97e49f9611ee1fcdd3
KarlaSancio <57644122+KarlaSancio@users.noreply.github.com>
2020-11-23 18:58:08 -0300

    tDCD

--- trabalhoprog2.c
+++ trabalhoprog2.c
@@ -2,23 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define max 12
+#define maxLinhas 10
+#define maxColunas 12
 
-
-typedef struct
+typedef struct DadosCidadeData
 {
-    //datas com 10 vetores para padrao de data;
     char DataCadastro[10];
     char DataObito[10];
-
-    //com no maximo 11 por ser a maior entrada esperada de caracteres;
     char Classificacao[11];
-
-    // sempre em maiusculo;
-    char Municipio[20];
-        
-    char IdadeNaDataNotificacao[20];
-
+    char Municipio[30];
+    char IdadeNaDataNotificacao[30];
     //vetores com 3 para sim, nao ou -;
     char ComorbidadePulmao[3];
     char ComorbidadeCardio[3];
@@ -26,23 +19,23 @@ typedef struct
     char ComorbidadeDiabetes[3];
     char ComorbidadeTabagismo[3];
     char ComorbidadeObesidade[3];
-    char FicouInternado[3];
+    char FicouInternado[13];
+}tDCD;
 
-} Dados;
+void LeArquivo(tDCD *DCD[]);
+void PegaInformacoes(tDCD *DCD[], char *linha[], int cont);
 
-typedef struct Dados dados;
+int main(){
+    tDCD DCD[maxLinhas];
 
+    LeArquivo(DCD);
 
-//Declaração das funções
-void LeArquivo();
-void PegaInformacoes(char *buffer);
-int QtdCasos(dados data);
-void ListaCidadesTopCasos(dados data, dados municipio);
+    return 0;
+}
 
-void LeArquivo(){
+void LeArquivo(tDCD *DCD[]){
     FILE *arquivo = fopen("covid19ES.csv", "r");
-    int cont = 0, i;
-
+    int nLinha = 0, i;
 
     if(arquivo==NULL){
         printf("Error\n");
@@ -51,43 +44,31 @@ void LeArquivo(){
 
     do //Lê o conteúdo do arquivo enquanto há algo para ser lido(até o fim do arquivo)
     {
-        char buffer[202363];//qtd de linhas do arquivo
-        fgets(buffer, 202363, (FILE*)arquivo);// pegando as strings de cada linha do arquivo
-        cont++;
-
-        if(cont != 1){
-            printf("%s", buffer);
-            PegaInformacoes(buffer);
+        char bufferLinha[202363];//qtd de linhas do arquivo
+        fgets(bufferLinha, 202363, (FILE*)arquivo);// pegando as strings de cada linha do arquivo
+        
+        if(nLinha != 0){
+            PegaInformacoes(DCD, bufferLinha, nLinha);
         }
+        nLinha++;
 
     //} while ((getc(arquivo)) != EOF);
-    } while ( cont != 10);
-
-    
+    } while ( nLinha != 10);
     
     fclose(arquivo);
 }
 
-void PegaInformacoes(char buffer[]){
-    int cont = 0, i;
+void PegaInformacoes(tDCD *DCD[], char *linha[], int nLinha){
     //Separa os itens da string que estão separados por vírgula
-    char *item = strtok(buffer, ",");
+    char *item = strtok(linha, ",");
 
-    while(item != NULL){
-        cont++;
-        printf(" %s\n",item);
-        // Armazena as informações do arquivo dentro dos vetores das structs
-        item = strtok(NULL, ",");
-
-    }
-}
-
-
-
-int main(){
-    
-    LeArquivo();
-
-
-    return 0;
+    for(int col = 1; col <= maxColunas; col++)
+    {
+        if(col == 1)
+            strcpy(DCD[col]->DataCadastro, item);
+        if(col == 2)
+            // DCD[col].DataObito = item;
+        // etc até o 12º item
+        item = strtok(NULL, ","); //pesquisar o que e
+    } 
 }
