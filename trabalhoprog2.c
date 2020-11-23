65425071d9809252c568c6c2a15d5c2b02fcbd21
KarlaSancio <57644122+KarlaSancio@users.noreply.github.com>
2020-11-23 16:50:09 -0300

    Leitura do arquivo

--- trabalhoprog2.c
+++ trabalhoprog2.c
@@ -5,7 +5,7 @@
 #define max 12
 
 
-typedef struct Dados
+typedef struct
 {
     //datas com 10 vetores para padrao de data;
     char DataCadastro[10];
@@ -28,28 +28,18 @@ typedef struct Dados
     char ComorbidadeObesidade[3];
     char FicouInternado[3];
 
-};
+} Dados;
 
 typedef struct Dados dados;
 
-struct Data
-{
-	int dia;
-	int mes;
-	int ano;
-};
-
-typedef struct Data dt;
 
 //Declaração das funções
-//Declaração das funções
-void LeArquivo(dados *dados[], int tam);
-void PegaInformacoes(char *buffer, dados *dados[], int tam, FILE * arquivo);
-intFiltroDatas(dados data[10]);
+void LeArquivo();
+void PegaInformacoes(char *buffer);
 int QtdCasos(dados data);
 void ListaCidadesTopCasos(dados data, dados municipio);
 
-void LeArquivo(dados *dados[], int tam){
+void LeArquivo(){
     FILE *arquivo = fopen("covid19ES.csv", "r");
     int cont = 0, i;
 
@@ -66,72 +56,37 @@ void LeArquivo(dados *dados[], int tam){
         cont++;
 
         if(cont != 1){
-            //printf("%s", buffer);
-            PegaInformacoes(buffer, dados, tam, arquivo);
-            
+            printf("%s", buffer);
+            PegaInformacoes(buffer);
         }
 
-    } while ((getc(arquivo)) != EOF);
+    //} while ((getc(arquivo)) != EOF);
+    } while ( cont != 10);
 
     
     
     fclose(arquivo);
 }
 
-void PegaInformacoes(char buffer[], dados *dados[], int tam, FILE * arquivo){
+void PegaInformacoes(char buffer[]){
     int cont = 0, i;
     //Separa os itens da string que estão separados por vírgula
     char *item = strtok(buffer, ",");
 
-    while(item !=  NULL){
+    while(item != NULL){
         cont++;
-        //fprintf(arquivo2, " %s\n",item);
+        printf(" %s\n",item);
         // Armazena as informações do arquivo dentro dos vetores das structs
-        for(i = 0; i <tam; i++){
-            fscanf(arquivo,"%s%s%s%s%s%s%s%s%s%s%s%s\n", &dados[i]->DataCadastro[10], &dados[i]->DataObito[10],
-                    &dados[i]->Classificacao[11], &dados[i]->Municipio[30], &dados[i]->IdadeNaDataNotificacao[30], &dados[i]->ComorbidadePulmao[3],
-                    &dados[i]->ComorbidadeCardio[3], &dados[i]->ComorbidadeRenal[3], &dados[i]->ComorbidadeDiabetes[3], &dados[i]->ComorbidadeTabagismo[3],
-                    &dados[i]->ComorbidadeObesidade[3], &dados[i]->FicouInternado[13]);
-        }
         item = strtok(NULL, ",");
-    }
-    for(i = 0; i < tam; i++){
-        printf("data: %s", dados[i]->DataCadastro);
+
     }
 }
 
-int FiltroDatas(dados DataCadastro[10]){
-    
-    int i,j;
-    dt filtro[10];
-
-    for (j=0, i = 0; i < 4; i++, j++){
-        filtro[j].ano = DataCadastro[i];
-    }
-        
-    for (j=6, i = 6; i <= 7; i++,j++){
-        filtro[j].mes = DataCadastro[i];
-    }
-        
-    for (j=9, i = 9; i <= 10 ; i++,j++){
-        filtro[j].dia = DataCadastro[i];
-    }
-    
-    
-    
-    
-}
 
 
 int main(){
-    dados *dates[max];
-    int max_size = max;
-    char data[10];
-
-    scanf("%s", &data[10]);
-
-    LeArquivo(dates, max_size);
-    FiltroDatas(data);
+    
+    LeArquivo();
 
 
     return 0;
