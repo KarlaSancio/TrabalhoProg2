429490e1573eb619c63746aa7031679e3978953e
KarlaSancio <57644122+KarlaSancio@users.noreply.github.com>
2020-11-24 20:00:38 -0300

    PegaInformacoesDaLinha

--- trabalhoprog2.c
+++ trabalhoprog2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define maxLinhas 100000
+#define maxLinhas 10
 #define maxColunas 12
 
 typedef struct DadosCidadeData
@@ -23,59 +23,83 @@ typedef struct DadosCidadeData
 }tDCD;
 tDCD DCD[maxLinhas];
 void LeArquivo();
-void PegaInformacoes(char linha[], int cont);
+void PegaInformacoesDaLinha(char linha[], int cont);
 
 int main(){
    
-    LeArquivo(DCD);
-    for (int i = 0; i < maxLinhas; i++)
-    {
-        // printf("%s\n", DCD[i].DataCadastro);
-    }
+    LeArquivo();
+    
     return 0;
 }
 
 void LeArquivo(){
-    FILE *arquivo = fopen("covid19ES.csv", "r");
+
+    FILE *arquivo = fopen("covid19ES2.csv", "r");
     int nLinha = 0, i;
+    char lixo1[20], lixo2[20];
+
     if(arquivo==NULL){
         printf("Error\n");
         exit (1);
     }
-    // (getc(arquivo)) != EOF
-    while ( nLinha != maxLinhas) //Lê o conteúdo do arquivo enquanto há algo para ser lido(até o fim do arquivo)
-    {
-        char bufferLinha[202363]; //qtd de linhas do arquivo
-        fgets(bufferLinha, 202363, (FILE*)arquivo); // pegando as strings de cada linha do arquivo
-        
-        if(nLinha != 0){
-            PegaInformacoes( bufferLinha, nLinha);
-        }
-        nLinha++;
+    for (i = 1; i <= maxLinhas; i++){
+        fscanf(arquivo, "%[^44], %[^44], %[^44], %[^44], %[^44], %[^44], %[^44], %[^44], %[^44], %[^44], %[^44], %[^44]",
+            DCD[nLinha].DataCadastro,
+            DCD[nLinha].DataObito,
+            DCD[nLinha].Classificacao,
+            DCD[nLinha].Municipio,
+            DCD[nLinha].IdadeNaDataNotificacao,
+            DCD[nLinha].ComorbidadePulmao,
+            DCD[nLinha].ComorbidadeCardio,
+            DCD[nLinha].ComorbidadeRenal,
+            DCD[nLinha].ComorbidadeDiabetes,
+            DCD[nLinha].ComorbidadeTabagismo,
+            DCD[nLinha].ComorbidadeObesidade,
+            DCD[nLinha].FicouInternado
+        );
     }
+
+    //char caracteresLinha[150];
+    //while ((fscanf(arquivo, "%[^\n]%*c", caracteresLinha)) != EOF) 
+    //{
+        //if(nLinha != 0){
+            //PegaInformacoesDaLinha( caracteresLinha, nLinha - 1);
+            //printf("%d %s\n", nLinha, caracteresLinha);
+        //}
+        //nLinha++;
+    //}
+    
     fclose(arquivo);
 }
 
-void PegaInformacoes( char linha[], int nLinha){
-    //Separa os itens da string que estão separados por vírgula
-    // printf("%s", linha);
-    int mLinha = nLinha - 1;
-    int letra = 0, comecaPalavra = 0;
-
-    while (linha[letra] != '\n')
+void PegaInformacoesDaLinha( char linha[], int mLinha){
+    // printf("%s %d\n", linha, mLinha);
+    int posLetra = 0, comecaPalavra = 0, numItem = 0;
+    
+    while (linha[posLetra] != '\n')
     {
-        // if(linha[letra] == ',')
-        if(letra == 10)
+        if(linha[posLetra] == ',')
         {
-            char word[letra-comecaPalavra];
-            for(int i = comecaPalavra; i < letra; i++) {
+            char word[posLetra-comecaPalavra];
+            for(int i = comecaPalavra; i < posLetra; i++) 
+            {
                 word[i - comecaPalavra] = linha[i];
             }
             
-            strcpy(DCD[mLinha].DataCadastro, word);
-            printf("%s\n", DCD[mLinha].DataCadastro);
-            comecaPalavra = letra + 1;
+            if(numItem == 0)
+            {
+                strcpy(DCD[mLinha].DataCadastro, word);
+                // printf("%s %s \n", word, DCD[mLinha].DataCadastro);
+            }
+            else if(numItem == 1)
+            {
+                strcpy(DCD[mLinha].DataObito, word);
+                // printf("%s\n", DCD[mLinha].DataObito);
+            }
+            numItem++;
+            comecaPalavra = posLetra + 1;
         }
-        letra++;
+        posLetra++;
     }
 }
+
