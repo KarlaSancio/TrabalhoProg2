61623e12400ef6c0415dafeff19a45e6937aa005
KarlaSancio <57644122+KarlaSancio@users.noreply.github.com>
2020-11-25 17:31:01 -0300

    Filtro de datas

--- trabalhoprog2.c
+++ trabalhoprog2.c
@@ -5,17 +5,28 @@
 #define maxLinhas 10
 #define maxColunas 12
 
+typedef struct Municipio
+{
+    int Casos;
+    char Nome[30];
+}tMun;
+
 typedef struct DadosCidadeData
 {
     char DataCadastro[30];
+    int DataCadastroAno;
+    int DataCadastroMes;
+    int DataCadastroDia;
     char DataObito[30];
+    int DataObitoAno;
+    int DataObitoMes;
+    int DataObitoDia;
     char Classificacao[30];
-    char Municipio[30];
+    tMun Municipio;
     char IdadeNaDataNotificacao[30];
     char IdadeAno[30];
     char IdadeMes[30];
     char IdadeDia[30];
-    //vetores com 3 para sim, nao ou -;
     char ComorbidadePulmao[30];
     char ComorbidadeCardio[30];
     char ComorbidadeRenal[30];
@@ -24,13 +35,19 @@ typedef struct DadosCidadeData
     char ComorbidadeObesidade[30];
     char FicouInternado[30];
 }tDCD;
-tDCD DCD[maxLinhas];
+
+tDCD DCD[maxLinhas]; //variavel que guarda as informacoes
+
 void LeArquivo();
 void PegaInformacoesDaLinha(char linha[], int cont);
+void TransformaDatas();
+void ListaCidadesComNCasos();
 
 int main(){
    
     LeArquivo();
+
+    ListaCidadesComNCasos();
     
     return 0;
 }
@@ -49,7 +66,7 @@ void LeArquivo(){
         DCD[nLinha].DataCadastro,
         DCD[nLinha].DataObito,
         DCD[nLinha].Classificacao,
-        DCD[nLinha].Municipio,
+        DCD[nLinha].Municipio.Nome,
         DCD[nLinha].IdadeNaDataNotificacao,
         DCD[nLinha].ComorbidadePulmao,
         DCD[nLinha].ComorbidadeCardio,
@@ -67,7 +84,7 @@ void LeArquivo(){
             DCD[nLinha].DataCadastro,
             DCD[nLinha].DataObito,
             DCD[nLinha].Classificacao,
-            DCD[nLinha].Municipio,
+            DCD[nLinha].Municipio.Nome,
             DCD[nLinha].IdadeAno,
             DCD[nLinha].IdadeMes,
             DCD[nLinha].IdadeDia,
@@ -84,11 +101,12 @@ void LeArquivo(){
         strcat(DCD[nLinha].IdadeNaDataNotificacao,DCD[nLinha].IdadeMes);
         strcat(DCD[nLinha].IdadeNaDataNotificacao,", ");
         strcat(DCD[nLinha].IdadeNaDataNotificacao,DCD[nLinha].IdadeDia);
+
         printf("%s %s %s %s %s %s %s %s %s %s %s %s\n", 
             DCD[nLinha].DataCadastro,
             DCD[nLinha].DataObito,
             DCD[nLinha].Classificacao,
-            DCD[nLinha].Municipio,
+            DCD[nLinha].Municipio.Nome,
             DCD[nLinha].IdadeNaDataNotificacao,
             DCD[nLinha].ComorbidadePulmao,
             DCD[nLinha].ComorbidadeCardio,
@@ -98,9 +116,56 @@ void LeArquivo(){
             DCD[nLinha].ComorbidadeObesidade,
             DCD[nLinha].FicouInternado
             );
-        printf("\n");
+         printf("\n");
         nLinha++;
     }
     
     fclose(arquivo);
+}
+
+void TransformaDatas(){ // Filtro de datas
+    int nLinha = 1;
+    while (nLinha <= maxLinhas)
+    {
+        sscanf(DCD[nLinha].DataCadastro, "%d-%d-%d", 
+        &DCD[nLinha].DataCadastroAno, 
+        &DCD[nLinha].DataCadastroMes, 
+        &DCD[nLinha].DataCadastroDia
+        );
+        
+        nLinha++;
+    }
+
+    int nLinha = 1;
+    while (nLinha <= maxLinhas)
+    {
+        sscanf(DCD[nLinha].DataObito, "%d-%d-%d", 
+        &DCD[nLinha].DataObitoAno, 
+        &DCD[nLinha].DataObitoMes, 
+        &DCD[nLinha].DataObitoDia
+        );
+        
+        nLinha++;
+    }
+}
+
+void ListaCidadesComNCasos(){
+
+    int N, i;
+    
+    char Comparador[15] = "Confirmados";
+
+    // scanf("%d",&N);
+
+    for (i = 0; i <= maxLinhas; i++){
+
+        if(strcmp(Comparador,DCD[i].Classificacao)==0){       
+            DCD[i].Municipio.Casos ++;
+            printf("%d_%s\n",DCD[i].Municipio.Casos,DCD[i].Municipio.Nome);
+        }
+
+        
+
+    }
+    
 }
